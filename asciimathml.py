a8a0406ebab45621dbd22657943f02f60e7f117d
Víctor López Juan <victor@lopezjuan.com>
2016-03-03 14:42:44 +0100

    Support quoted strings

--- asciimathml.py
+++ asciimathml.py
@@ -46,6 +46,7 @@ def El(tag, text=None, *children, **attrib):
     return element
 
 number_re = re.compile('-?(\d+\.(\d+)?|\.?\d+)')
+quoted_string_re = re.compile(r'"([^"]*)"')
 
 def strip_parens(n):
     if n.tag == 'mrow':
@@ -335,6 +336,11 @@ def parse_m(s, required=False):
 
     if s == '':
         return '', El('mi', u'\u25a1') if required else None
+
+    m = quoted_string_re.match(s)
+    if m:
+        text = m.group(1)
+        return s[m.end():], El('mrow', El('mtext', text))
 
     m = number_re.match(s)
 
